f44f7ce461a998acf78ae2ebeee1b618c554ded6
kurinskaya <felisinka@gmail.com>
2024-06-12 21:05:05 +0400

    Triangle equality changed

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,8 +1,34 @@
 package ru.stqa.geometry.figures;
 
-public record Triangle (double sideA,double sideB,double sideC){
+import java.util.Objects;
 
-public Triangle {
+public record Triangle (double sideA, double sideB, double sideC){
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(sideA, triangle.sideA) == 0 && Double.compare(sideB, triangle.sideB) == 0 && Double.compare(sideC, triangle.sideC) == 0)
+                ||
+                (Double.compare(sideA, triangle.sideC) == 0 && Double.compare(sideB, triangle.sideB) == 0 && Double.compare(sideC, triangle.sideA) == 0)
+                ||
+                (Double.compare(sideA, triangle.sideB) == 0 && Double.compare(sideB, triangle.sideA) == 0 && Double.compare(sideC, triangle.sideC) == 0)
+                ||
+                (Double.compare(sideA, triangle.sideA) == 0 && Double.compare(sideB, triangle.sideC) == 0 && Double.compare(sideC, triangle.sideB) == 0)
+                ||
+                (Double.compare(sideA, triangle.sideB) == 0 && Double.compare(sideB, triangle.sideC) == 0 && Double.compare(sideC, triangle.sideA) == 0)
+                ||
+                (Double.compare(sideA, triangle.sideC) == 0 && Double.compare(sideB, triangle.sideA) == 0 && Double.compare(sideC, triangle.sideB) == 0)
+                ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sideA, sideB, sideC);
+    }
+
+    public Triangle {
     if (sideA<0||sideB<0||sideC<0||sideA+sideB<sideC||sideA+sideC<sideB||sideB+sideC<sideA) {
       throw new IllegalArgumentException("Triangle side should be non-negative");
     }
--- sandbox/src/test/java/ru/stqa/geometry/figures/triangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/triangleTests.java
@@ -5,64 +5,81 @@ import org.junit.jupiter.api.Test;
 
 public class TriangleTests {
 
-        @Test
-        void canCalculateArea(){
-            var t = new Triangle(10.0,6.0,8.0);
-            Assertions.assertEquals(24.0, t.area());
-            t = new Triangle(3.0,4.0,5.0);
-            Assertions.assertEquals(6.0, t.area());
-        }
-        @Test
-        void canCalculatePerimeter(){
-            var t = new Triangle(10.0,6.0,8.0);
-            Assertions.assertEquals(24.0, t.perimeter());
-            t = new Triangle(3.0,4.0,5.0);
-            Assertions.assertEquals(12.0, t.perimeter());
-        }
-
-        @Test
-        void cannotCreateTriangleWithNegativeSideA(){
-           try {
-               new Triangle(-5, 1, 3);
-               Assertions.fail();
-           }
-           catch (IllegalArgumentException exception){
-               //ok
-           }
-        }
-
-        @Test
-        void cannotCreateTriangleWithNegativeSideB(){
-            try {
-            new Triangle(5, -1,3);
-            Assertions.fail();
-            }
-            catch (IllegalArgumentException exception){
-                //ok
-            }
-        }
-
-        @Test
-        void cannotCreateTriangleWithNegativeSideC(){
-            try {
-            new Triangle(5, 1,-3);
-            Assertions.fail();
-            }
-            catch (IllegalArgumentException exception){
-                //ok
-            }
-        }
+    @Test
+    void canCalculateArea() {
+        var t = new Triangle(10.0, 6.0, 8.0);
+        Assertions.assertEquals(24.0, t.area());
+        t = new Triangle(3.0, 4.0, 5.0);
+        Assertions.assertEquals(6.0, t.area());
+    }
 
     @Test
-    void cannotCreateTriangleWithBrokenSideInequality(){
+    void canCalculatePerimeter() {
+        var t = new Triangle(10.0, 6.0, 8.0);
+        Assertions.assertEquals(24.0, t.perimeter());
+        t = new Triangle(3.0, 4.0, 5.0);
+        Assertions.assertEquals(12.0, t.perimeter());
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSideA() {
         try {
-            new Triangle(5, 1,3);
+            new Triangle(-5, 1, 3);
             Assertions.fail();
-        }
-        catch (IllegalArgumentException exception){
+        } catch (IllegalArgumentException exception) {
             //ok
         }
     }
 
+    @Test
+    void cannotCreateTriangleWithNegativeSideB() {
+        try {
+            new Triangle(5, -1, 3);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
     }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSideC() {
+        try {
+            new Triangle(5, 1, -3);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithBrokenSideInequality() {
+        try {
+            new Triangle(5, 1, 3);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //ok
+        }
+    }
+
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(10.0, 6.0, 8.0);
+        var t2 = new Triangle(10.0, 6.0, 8.0);
+        Assertions.assertEquals(t1,t2);
+    }
+
+    @Test
+    void testEquality2() {
+        var t1 = new Triangle(10.0, 6.0, 8.0);
+        var t2 = new Triangle(6.0, 10.0, 8.0);
+        Assertions.assertEquals(t1,t2);
+    }
+
+    @Test
+    void testEquality3() {
+        var t1 = new Triangle(10.0, 6.0, 8.0);
+        var t2 = new Triangle(8.0, 10.0, 6.0);
+        Assertions.assertEquals(t1,t2);
+    }
+}
 
