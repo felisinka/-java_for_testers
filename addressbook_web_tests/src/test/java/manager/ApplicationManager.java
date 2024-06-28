522f447bb154140a856a3fec0385189f9ea2ef77
Ekaterina <felisinka@gmail.com>
2024-06-28 10:57:55 +0400

    Browser parameter added

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,10 +1,14 @@
 package manager;
 
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.GeckoDriverService;
 
 public class ApplicationManager {
     protected WebDriver driver;
@@ -25,19 +29,30 @@ public GroupHelper groups(){
     return groups;
 }
 
-    public void init() {
+    public void init(String browser) {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-debugging-port=9222");
-            //options.addArguments("start-maximized"); // open Browser in maximized mode
-            //options.addArguments("disable-infobars"); // disabling infobars
-            // options.addArguments("--disable-extensions"); // disabling extensions
-            //options.addArguments("--disable-gpu"); // applicable to windows os only
-            // options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            //options.addArguments("--no-sandbox"); // Bypass OS security model
-            driver = new ChromeDriver(options);
+            if ("chrome".equals(browser)) {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-debugging-port=9222");
+                //options.addArguments("start-maximized"); // open Browser in maximized mode
+                //options.addArguments("disable-infobars"); // disabling infobars
+                // options.addArguments("--disable-extensions"); // disabling extensions
+                //options.addArguments("--disable-gpu"); // applicable to windows os only
+                // options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+                //options.addArguments("--no-sandbox"); // Bypass OS security model
+                driver = new ChromeDriver(options);
+
+            } else if ("firefox".equals(browser))
+            {
+                // put gecko driver to the path. it starts selenium, not firefox!
+                System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+                driver = new FirefoxDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
+            //firefox driver works only with url to index.php. otherwise failed with Unable to locate element: *[name='user']
+            driver.get("http://localhost/addressbook/index.php");
             session().login("admin", "secret");
         }
     }
