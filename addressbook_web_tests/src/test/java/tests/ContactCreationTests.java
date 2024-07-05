22df366ad42d1da4e9f0724498c841360cdcfd3e
Ekaterina <felisinka@gmail.com>
2024-07-05 12:55:34 +0400

    Contact Creation tests were parameterized

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -2,26 +2,58 @@ package tests;
 
 import model.ContactData;
 import model.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ContactCreationTests extends TestBase{
 
-
-  @Test
-  public void canCreateContact() {
-    app.contacts().createContact(new ContactData("First Name","Last Name","Test Address","email@email.com","+79161307546"));
+  public static List<ContactData> contactProvider() {
+    var result = new ArrayList<ContactData>();
+    for (var name: List.of("","contact name")) {
+      for (var lastName : List.of("", "contact last name")) {
+        for (var address : List.of("", "contact address")) {
+          for (var email : List.of("", "contact email")) {
+            for (var homePhone : List.of("", "+79161307546")) {
+              result.add(new ContactData(name, lastName, address, email, homePhone));
+            }
+          }
+        }
+      }
+    }
+    for (int i=0;i<5;i++){
+      result.add(new ContactData(randomString(i*10),randomString(i*10),randomString(i*10),randomString(i*10),randomString(i*10)));
+    }
+    return result;
   }
 
-  @Test
-  public void canCreateContactWithEmptyName() {
-    app.contacts().createContact(new ContactData());
-
+  @ParameterizedTest
+  @MethodSource("contactProvider")
+  public void canCreateMultipleContacts(ContactData contact) {
+    int contactCount = app.contacts().getCount();
+    app.contacts().createContact(contact);
+    int newContactCount = app.contacts().getCount();
+    Assertions.assertEquals(contactCount+1,newContactCount);
   }
 
-  @Test
-  public void canCreateContactWithFirstNameOnly() {
-    app.contacts().createContact(new ContactData().withFirstName("Some name"));
 
+  public static List<ContactData> negativeContactProvider() {
+    var result = new ArrayList<ContactData>(List.of(new ContactData().withFirstName("Some name'")));
+    return result;
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("negativeContactProvider")
+  public void cannotCreateContact(ContactData contact) {
+    int contactCount = app.contacts().getCount();
+    app.contacts().createContact(contact);
+    int newContactCount = app.contacts().getCount();
+    Assertions.assertEquals(contactCount,newContactCount);
   }
 }
