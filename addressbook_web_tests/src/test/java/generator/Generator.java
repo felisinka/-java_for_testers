90678c15fb1b9f756220c5feb6bff24d40464725
Ekaterina <felisinka@gmail.com>
2024-07-16 08:27:31 +0400

    If for file format changed

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -87,10 +87,10 @@ public class Generator {
             ObjectMapper mapper = new ObjectMapper();
             mapper.enable(SerializationFeature.INDENT_OUTPUT);
             mapper.writeValue(new File(output), data);
-        } if ("yaml".equals(format)){
+        } else if ("yaml".equals(format)){
             var mapper = new YAMLMapper();
             mapper.writeValue(new File(output),data);
-        } if ("xml".equals(format)){
+        } else if ("xml".equals(format)){
             var mapper = new XmlMapper();
             mapper.writeValue(new File(output),data);
         } else {
